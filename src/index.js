6104ca38d1f9c0af6aee212f709eda248a90b32d
Anton Abyzov <anton.abyzov@gmail.com>
2019-01-08 17:30:21 +0300

    changing package.json

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
-import './assets/styles/css/index.css';
+//import './assets/styles/css/index.css';
 import 'react-redux-toastr/lib/css/react-redux-toastr.min.css';
 
 ReactDOM.render(
 
     <I18nextProvider i18n={i18n}>
         <App />
-    </I18nextProvider>
+</I18nextProvider>
 
     ,
     document.getElementById('root'));
